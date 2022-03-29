a61f37a6a756cdd9fbfc248c442be3f993448844
coderNini <99504328+coderNini@users.noreply.github.com>
2022-03-29 11:39:10 -0700

    Create Heartless+HolyGrail.rb

---
+++ Heartless+HolyGrail.rb
@@ -0,0 +1,109 @@
+# Holy Grail by Justin Timberlake ft. Jay Z
+
+use_bpm 72
+
+use_synth :piano
+
+heartless = "C:/Users/nia_robinson/Desktop/Heartless (ACAPELLA).wav.wav"
+
+
+
+
+
+
+
+sample heartless
+sleep 13.7
+play :f5, sustain_level: 15, amp: 0.25
+sleep 0.75
+play :a5, sustain_level: 3, amp: 1
+sleep 0.5
+play :c6, sustain_level: 2, amp: 2
+sleep 1
+play :c5, amp: 4
+sleep 0.25
+play :c5, amp: 4
+sleep 0.25
+play :a5, sustain_level: 20, amp: 8
+sleep 0.75
+play :a5, amp: 10
+sleep 0.25
+play :a5, sustain_level: 2, amp: 10
+sleep 0.25
+play :g5, sustain_level: 2, amp: 10
+sleep 0.5
+play :f5, sustain_level: 2, amp: 10
+sleep 0.25
+play :g5, sustain_level: 20, amp: 10
+sleep 0.75
+play :c5, amp: 10
+sleep 0.25
+play :c5, sustain_level: 15, amp: 10
+sleep 0.75
+play :c5, amp: 10
+sleep 0.25
+play :g5, amp: 10
+sleep 0.5
+play :g5, amp: 10
+sleep 0.5
+play :a5, amp: 10
+sleep 0.5
+play :e5, amp: 10
+sleep 0.25
+play :f5, sustain_level: 15, amp: 10
+sleep 2
+play :f5, sustain_level: 15, amp: 10
+sleep 0.5
+play :f5, amp: 10
+sleep 0.25
+play :f5, amp: 10
+sleep 0.25
+play :f5, amp: 10
+sleep 0.25
+play :f5, amp: 10
+sleep 0.25
+play :f5, amp: 10
+sleep 0.25
+play :g5, sustain_level: 15, amp: 10
+sleep 0.75
+play :c5, amp: 10
+sleep 0.25
+play :c5, sustain_level: 15, amp: 10
+sleep 1
+play :c5, amp: 10
+sleep 0.25
+play :c5, amp: 10
+sleep 0.5
+play :c5, amp: 10
+sleep 0.5
+play :bb4, amp: 10
+sleep 0.25
+play :a4, amp: 10
+sleep 0.5
+play :bb4, sustain_level: 30, amp: 10
+sleep 0.25
+play :a4, sustain_level: 15, amp: 10
+sleep 1
+play :c5, amp: 10 #pattern 1
+sleep 0.25
+play :c5, amp: 10
+sleep 0.25
+play :c5, amp: 10
+sleep 0.25
+play :g5, sustain_level: 20, amp: 10
+sleep 1.25
+play :c5, amp: 10 #pattern 1  x2
+sleep 0.25
+play :c5, amp: 10
+sleep 0.25
+play :c5, amp: 10
+sleep 0.25
+play :g5, sustain_level: 20, amp: 10
+sleep 1.25
+play :c5, amp: 10 #pattern 1  x3
+sleep 0.25
+play :c5, amp: 10
+sleep 0.25
+play :c5, amp: 10
+sleep 0.25
+play :g5, sustain_level: 20, amp: 10  # ending @ "One day you caarrree ..."
